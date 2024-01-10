cbf9131c7eb36f2c7ec0cdb4c9cbb910eaff1fa7
Luu Hung <hungluu344@gmail.com>
2024-01-10 08:35:09 +0700

    Added Rainy Square

---
+++ RainySquare/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace RainySquare
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = 8;
+            int[,] matrix = new int[n, n];
+            //Init first line
+            int count = 1;
+            matrix[0, 0] = 1;
+            for (int i = 1; i < n; i++)
+            {
+                matrix[0,i] = matrix[0, i - 1] + count;
+                count++;
+            }
+            //Init left crossing
+            for (int i = 1; i < n; i++)
+            {
+                int counter = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    matrix[j, i - j] = matrix[0,i] + 1 + counter;
+                    counter++;
+                }
+            }
+            //Init right column
+            count = n;
+            for (int i = 1; i < n; i++)
+            {
+                matrix[i, n - 1] = matrix[i - 1, n - 1] + count;
+                count--;
+            }
+            //Init right crossing
+            for (int i = 1; i < n; i++)//Column
+            {
+                int counter = 0;
+
+                for (int j = 1; j <= n - i - 1; j++)
+                {
+                    Console.WriteLine($"[{n-i},{n-j-1}] -> [{n-i-1 - counter},{n-1}] = {matrix[n - i - 1 - counter, n-1]} + {1 + counter}");
+                    matrix[n - i,n - j - 1] = matrix[n - i - 1 - counter, n-1] + 1 + counter;
+                    counter++;
+                }
+            }
+            /*
+            //Init right crossing
+            for (int i = 1; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i; j++)
+                {
+                    matrix[i, j] = matrix[i - 1, j + 1] + 1;
+                }
+            }
+            //Init right column
+            count = n;
+            for (int i = 1; i < n - 1; i++)
+            {
+                matrix[i, n - 1] = matrix[i - 1, n - 1] + count;
+                count--;
+            }
+            //Init left crossing
+            for (int i = n - 2; i > 1; i--)
+            {
+                for (int j = n - 2; j > 1; j--)
+                {
+                    Console.WriteLine(matrix[i - 1, j + 1]);
+                    matrix[i, j] = matrix[i - 1, j + 1] + 1;
+                }
+            }
+            //Init last line
+            count = 1;
+            matrix[n-1, n-1] = n*n;
+            for (int i = n - 1 - 1; i >= 0; i--)
+            {
+                matrix[n - 1, i] = matrix[n - 1, i + 1] - count;
+                count++;
+            }
+            */
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("{0,4} ", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
